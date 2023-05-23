dc8dfbc2ead99bbf3be48a8309572ce48216729c
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-23 20:50:01 +0200

    Show which architecture instruction is used
    This is just so that I can test that the stuff is actually being
    used on my cloud VM's.
    
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 
 int32_t main(int32_t argument_count, char **arguments)
 {
-    printf("Hello, World\n");
+// printf("Hello, World\n");
+#if defined(__AVX512F__)
+    printf("Using AVX 512\n");
+#elif defined(__ARM_NEON)
+    printf("Using NEON\n");
+#else
+    printf("Using Standard\n");
+#endif
 
     return 0;
 }
