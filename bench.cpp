2e393057f1f83f4444e35a0891ac62badd58518d
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-28 00:34:45 +0200

    Make benchmark take more samples
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- bench.cpp
+++ bench.cpp
@@ -16,8 +16,8 @@
 
 CELERO_MAIN
 
-static constexpr int32_t IMAGE_WIDTH = 2048;
-static constexpr int32_t IMAGE_HEIGHT = 2048;
+static constexpr int32_t IMAGE_WIDTH = 8192;
+static constexpr int32_t IMAGE_HEIGHT = 8192;
 static uint32_t image[IMAGE_WIDTH * IMAGE_HEIGHT] = {0};
 
 static point2d_t v0{IMAGE_WIDTH, IMAGE_HEIGHT};
@@ -30,19 +30,19 @@ class demo_simple_fixture : public celero::TestFixture
 {
 };
 
-BASELINE_F(demo_simple, general, demo_simple_fixture, 100, 1000000)
+BASELINE_F(demo_simple, general, demo_simple_fixture, 1000, 1000000)
 {
     celero::DoNotOptimizeAway([&]()
                               { draw_triangle_general(image, IMAGE_WIDTH, IMAGE_HEIGHT, v0, v1, v2, color); });
 }
 
-BENCHMARK_F(demo_simple, optimized_1, demo_simple_fixture, 100, 1000000)
+BENCHMARK_F(demo_simple, optimized_1, demo_simple_fixture, 1000, 1000000)
 {
     celero::DoNotOptimizeAway([&]()
                               { draw_triangle_optimized_1(image, IMAGE_WIDTH, IMAGE_HEIGHT, v0, v1, v2, color); });
 }
 
-BENCHMARK_F(demo_simple, optimized_2, demo_simple_fixture, 100, 1000000)
+BENCHMARK_F(demo_simple, optimized_2, demo_simple_fixture, 1000, 1000000)
 {
     celero::DoNotOptimizeAway([&]()
                               { draw_triangle_optimized_2(image, IMAGE_WIDTH, IMAGE_HEIGHT, v0, v1, v2, color); });
@@ -66,7 +66,7 @@ BENCHMARK_F(demo_simple, SVE, demo_simple_fixture, 32, 16384)
 
 #if defined(__AVX2__)
 
-BENCHMARK_F(demo_simple, AVX2, demo_simple_fixture, 100, 1000000)
+BENCHMARK_F(demo_simple, avx2, demo_simple_fixture, 1000, 1000000)
 {
     celero::DoNotOptimizeAway([&]()
                               { draw_triangle_avx2(image, IMAGE_WIDTH, IMAGE_HEIGHT, v0, v1, v2, color); });
