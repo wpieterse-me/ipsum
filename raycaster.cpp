6ea23c34b8d909189cc0ada3b5c61e9b22551ee9
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-27 15:10:12 +0200

    Draw rays for raycaster for FOV
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- raycaster.cpp
+++ raycaster.cpp
@@ -72,6 +72,8 @@ float player_x = 3.456;
 float player_y = 2.345;
 float player_a = 1.523;
 
+const float fov = M_PI / 3.0f;
+
 int32_t main(int32_t argument_count, char **arguments)
 {
     const size_t win_w = 512;
@@ -110,20 +112,24 @@ int32_t main(int32_t argument_count, char **arguments)
 
     draw_rectangle(framebuffer, win_w, win_h, player_x * rect_w, player_y * rect_h, 5, 5, pack_color(255, 255, 255));
 
-    for (float t = 0; t < 20; t += 0.05)
+    for (size_t i = 0; i < win_w; i++)
     {
-        float cx = player_x + t * cos(player_a);
-        float cy = player_y + t * sin(player_a);
-
-        if (map[int(cx) + int(cy) * map_w] != ' ')
+        float angle = player_a - fov / 2 + fov * i / float(win_w);
+        for (float t = 0; t < 20; t += 0.05)
         {
-            break;
+            float cx = player_x + t * cos(angle);
+            float cy = player_y + t * sin(angle);
+
+            if (map[int(cx) + int(cy) * map_w] != ' ')
+            {
+                break;
+            }
+
+            size_t pix_x = cx * rect_w;
+            size_t pix_y = cy * rect_h;
+
+            framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
         }
-
-        size_t pix_x = cx * rect_w;
-        size_t pix_y = cy * rect_h;
-
-        framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
     }
 
     drop_ppm_image("out.ppm", framebuffer, win_w, win_h);
