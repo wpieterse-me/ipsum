744c2e7e2a6bc36d32a83996221941738e06332a
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-27 17:59:40 +0200

    Fix up bench to run on AVX512 and NEON
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- bench.cpp
+++ bench.cpp
@@ -64,30 +64,36 @@ BASELINE_F(DemoSimple, Baseline, DemoSimpleFixture, 16, 4096)
 
 #if defined(__ARM_NEON)
 
-uint32_t GetPaletteIndex_NEON(Color color)
-{
-    auto v = vcreate_u8(0x0102030405060708);
-
-    return vget_lane_u8(v, 0);
-}
-
 BENCHMARK_F(DemoSimple, NEON, DemoSimpleFixture, 16, 4096)
 {
-    celero::DoNotOptimizeAway(GetPaletteIndex_NEON(Color::Blue));
+    static constexpr int32_t window_width = 512;
+    static constexpr int32_t window_height = 512;
+
+    uint32_t framebuffer[512 * 512] = {0};
+    Point2D a{window_width, window_height};
+    Point2D b{0, window_height};
+    Point2D c{0, 0};
+
+    celero::DoNotOptimizeAway([&]()
+                              { DrawTriangle(window_width, window_height, framebuffer, a, b, c); });
 }
 
 #endif
 
 #if defined(__ARM_FEATURE_SVE)
 
-uint32_t GetPaletteIndex_SVE(Color color)
-{
-    return svcntd();
-}
-
 BENCHMARK_F(DemoSimple, SVE, DemoSimpleFixture, 16, 4096)
 {
-    celero::DoNotOptimizeAway(GetPaletteIndex_SVE(Color::Blue));
+    static constexpr int32_t window_width = 512;
+    static constexpr int32_t window_height = 512;
+
+    uint32_t framebuffer[512 * 512] = {0};
+    Point2D a{window_width, window_height};
+    Point2D b{0, window_height};
+    Point2D c{0, 0};
+
+    celero::DoNotOptimizeAway([&]()
+                              { DrawTriangle(window_width, window_height, framebuffer, a, b, c); });
 }
 
 #endif
@@ -202,18 +208,18 @@ BENCHMARK_F(DemoSimple, AVX2, DemoSimpleFixture, 16, 4096)
 
 #if defined(__AVX512F__)
 
-int32_t GetPaletteIndex_AVX512(Color color)
-{
-    const __m512i vec = _mm512_set1_epi32((uint32_t)color);
-    const __m512i lookup = _mm512_setr_epi32((uint32_t)Color::Red, (uint32_t)Color::Green, (uint32_t)Color::Blue, (uint32_t)Color::White, (uint32_t)Color::Gray, (uint32_t)Color::Black, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
-    const __mmask16 mask = _mm512_cmpeq_epi32_mask(vec, lookup);
-    const uint32_t mask_b = mask | 0x40;
-    return __builtin_ctz(mask_b);
-}
-
 BENCHMARK_F(DemoSimple, AVX512, DemoSimpleFixture, 16, 4096)
 {
-    celero::DoNotOptimizeAway(GetPaletteIndex_AVX512(Color::Blue));
+    static constexpr int32_t window_width = 512;
+    static constexpr int32_t window_height = 512;
+
+    uint32_t framebuffer[512 * 512] = {0};
+    Point2D a{window_width, window_height};
+    Point2D b{0, window_height};
+    Point2D c{0, 0};
+
+    celero::DoNotOptimizeAway([&]()
+                              { DrawTriangle(window_width, window_height, framebuffer, a, b, c); });
 }
 
 #endif
