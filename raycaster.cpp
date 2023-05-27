7236980c75c5ed7b4acfd4da0b1dfc1928efa886
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-27 15:23:07 +0200

    Add first 3D view
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- raycaster.cpp
+++ raycaster.cpp
@@ -43,6 +43,11 @@ void draw_rectangle(std::vector<uint32_t> &image, const size_t img_w, const size
         {
             size_t cx = x + i;
             size_t cy = y + j;
+            if (cx >= img_w || cy >= img_h)
+            {
+                continue;
+            }
+
             image[cx + cy * img_w] = color;
         }
     }
@@ -76,23 +81,11 @@ const float fov = M_PI / 3.0f;
 
 int32_t main(int32_t argument_count, char **arguments)
 {
-    const size_t win_w = 512;
+    const size_t win_w = 1024;
     const size_t win_h = 512;
-    std::vector<uint32_t> framebuffer(win_w * win_h, 255);
+    std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(255, 255, 255));
 
-    for (size_t j = 0; j < win_h; j++)
-    {
-        for (size_t i = 0; i < win_w; i++)
-        {
-            uint8_t r = 255 * j / float(win_h);
-            uint8_t g = 255 * i / float(win_w);
-            uint8_t b = 0;
-
-            framebuffer[i + j * win_w] = pack_color(r, g, b);
-        }
-    }
-
-    const size_t rect_w = win_w / map_w;
+    const size_t rect_w = win_w / (map_w * 2);
     const size_t rect_h = win_h / map_h;
     for (size_t j = 0; j < map_h; j++)
     {
@@ -110,25 +103,25 @@ int32_t main(int32_t argument_count, char **arguments)
         }
     }
 
-    draw_rectangle(framebuffer, win_w, win_h, player_x * rect_w, player_y * rect_h, 5, 5, pack_color(255, 255, 255));
-
-    for (size_t i = 0; i < win_w; i++)
+    for (size_t i = 0; i < win_w / 2; i++)
     {
-        float angle = player_a - fov / 2 + fov * i / float(win_w);
-        for (float t = 0; t < 20; t += 0.05)
+        float angle = player_a - fov / 2.0f + fov * i / float(win_w / 2.0f);
+        for (float t = 0.0f; t < 20.0f; t += 0.05f)
         {
             float cx = player_x + t * cos(angle);
             float cy = player_y + t * sin(angle);
 
-            if (map[int(cx) + int(cy) * map_w] != ' ')
-            {
-                break;
-            }
-
             size_t pix_x = cx * rect_w;
             size_t pix_y = cy * rect_h;
 
-            framebuffer[pix_x + pix_y * win_w] = pack_color(255, 255, 255);
+            framebuffer[pix_x + pix_y * win_w] = pack_color(100, 100, 100);
+
+            if (map[int(cx) + int(cy) * map_w] != ' ')
+            {
+                size_t column_height = win_h / t;
+                draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 255, 255));
+                break;
+            }
         }
     }
 
