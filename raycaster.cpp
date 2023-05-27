8be0c81027fcc5b3dd8b5f63430b562e2ceed7af
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-27 15:29:27 +0200

    Animation of 3D view achieved
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- raycaster.cpp
+++ raycaster.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255)
 {
@@ -53,78 +55,87 @@ void draw_rectangle(std::vector<uint32_t> &image, const size_t img_w, const size
     }
 }
 
-const size_t map_w = 16;
-const size_t map_h = 16;
-
-const char map[] = "0000222222220000"
-                   "1              0"
-                   "1      11111   0"
-                   "1     0        0"
-                   "0     0  1110000"
-                   "0     3        0"
-                   "0   10000      0"
-                   "0   0   11100  0"
-                   "0   0   0      0"
-                   "0   0   1  00000"
-                   "0       1      0"
-                   "2       1      0"
-                   "0       0      0"
-                   "0 0000000      0"
-                   "0              0"
-                   "0002222222200000"; // our game map
-
-float player_x = 3.456;
-float player_y = 2.345;
-float player_a = 1.523;
-
-const float fov = M_PI / 3.0f;
-
 int32_t main(int32_t argument_count, char **arguments)
 {
     const size_t win_w = 1024;
     const size_t win_h = 512;
-    std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(255, 255, 255));
-
+    const size_t map_w = 16;
+    const size_t map_h = 16;
     const size_t rect_w = win_w / (map_w * 2);
     const size_t rect_h = win_h / map_h;
-    for (size_t j = 0; j < map_h; j++)
+
+    const char map[] = "0000222222220000"
+                       "1              0"
+                       "1      11111   0"
+                       "1     0        0"
+                       "0     0  1110000"
+                       "0     3        0"
+                       "0   10000      0"
+                       "0   0   11100  0"
+                       "0   0   0      0"
+                       "0   0   1  00000"
+                       "0       1      0"
+                       "2       1      0"
+                       "0       0      0"
+                       "0 0000000      0"
+                       "0              0"
+                       "0002222222200000"; // our game map
+
+    float player_x = 3.456;
+    float player_y = 2.345;
+    float player_a = 1.523;
+
+    const float fov = M_PI / 3.0f;
+
+    for (size_t frame = 0; frame < 360; frame++)
     {
-        for (size_t i = 0; i < map_w; i++)
+        std::stringstream ss;
+        ss << std::setfill('0') << std::setw(5) << frame << ".ppm";
+
+        player_a += 2 * M_PI / 360;
+
+        std::vector<uint32_t> framebuffer(win_w * win_h, pack_color(0, 0, 0));
+
+        for (size_t j = 0; j < map_h; j++)
         {
-            if (map[i + j * map_w] == ' ')
+            for (size_t i = 0; i < map_w; i++)
             {
-                continue;
+                if (map[i + j * map_w] == ' ')
+                {
+                    continue;
+                }
+
+                size_t rect_x = i * rect_w;
+                size_t rect_y = j * rect_h;
+
+                draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, pack_color(255, 0, 0));
             }
-
-            size_t rect_x = i * rect_w;
-            size_t rect_y = j * rect_h;
-
-            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, pack_color(0, 255, 255));
         }
+
+        for (size_t i = 0; i < win_w / 2; i++)
+        {
+            float angle = player_a - fov / 2.0f + fov * i / float(win_w / 2.0f);
+            for (float t = 0.0f; t < 20.0f; t += 0.01f)
+            {
+                float cx = player_x + t * cos(angle);
+                float cy = player_y + t * sin(angle);
+
+                size_t pix_x = cx * rect_w;
+                size_t pix_y = cy * rect_h;
+
+                framebuffer[pix_x + pix_y * win_w] = pack_color(0, 255, 0);
+
+                if (map[int(cx) + int(cy) * map_w] != ' ')
+                {
+                    size_t column_height = win_h / t;
+                    draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 0, 255));
+                    break;
+                }
+            }
+        }
+
+        drop_ppm_image(ss.str(), framebuffer, win_w, win_h);
     }
 
-    for (size_t i = 0; i < win_w / 2; i++)
-    {
-        float angle = player_a - fov / 2.0f + fov * i / float(win_w / 2.0f);
-        for (float t = 0.0f; t < 20.0f; t += 0.05f)
-        {
-            float cx = player_x + t * cos(angle);
-            float cy = player_y + t * sin(angle);
-
-            size_t pix_x = cx * rect_w;
-            size_t pix_y = cy * rect_h;
-
-            framebuffer[pix_x + pix_y * win_w] = pack_color(100, 100, 100);
-
-            if (map[int(cx) + int(cy) * map_w] != ' ')
-            {
-                size_t column_height = win_h / t;
-                draw_rectangle(framebuffer, win_w, win_h, win_w / 2 + i, win_h / 2 - column_height / 2, 1, column_height, pack_color(0, 255, 255));
-                break;
-            }
-        }
-    }
-
-    drop_ppm_image("out.ppm", framebuffer, win_w, win_h);
     return 0;
 }
