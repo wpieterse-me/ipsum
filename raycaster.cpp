af4ec6ca4e1ec5e120f073c0c151e7beb04689d6
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-27 15:02:36 +0200

    Draw map to output image
    Still in 2D, but we can see the map as it is expressed in code.
    
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- raycaster.cpp
+++ raycaster.cpp
@@ -34,6 +34,39 @@ void drop_ppm_image(const std::string filename, const std::vector<uint32_t> &ima
     ofs.close();
 }
 
+void draw_rectangle(std::vector<uint32_t> &image, const size_t img_w, const size_t img_h, const size_t x, const size_t y, const size_t w, const size_t h, const uint32_t color)
+{
+    for (size_t i = 0; i < w; i++)
+    {
+        for (size_t j = 0; j < h; j++)
+        {
+            size_t cx = x + i;
+            size_t cy = y + j;
+            image[cx + cy * img_w] = color;
+        }
+    }
+}
+
+const size_t map_w = 16;
+const size_t map_h = 16;
+
+const char map[] = "0000222222220000"
+                   "1              0"
+                   "1      11111   0"
+                   "1     0        0"
+                   "0     0  1110000"
+                   "0     3        0"
+                   "0   10000      0"
+                   "0   0   11100  0"
+                   "0   0   0      0"
+                   "0   0   1  00000"
+                   "0       1      0"
+                   "2       1      0"
+                   "0       0      0"
+                   "0 0000000      0"
+                   "0              0"
+                   "0002222222200000"; // our game map
+
 int32_t main(int32_t argument_count, char **arguments)
 {
     const size_t win_w = 512;
@@ -49,6 +82,24 @@ int32_t main(int32_t argument_count, char **arguments)
             uint8_t b = 0;
 
             framebuffer[i + j * win_w] = pack_color(r, g, b);
+        }
+    }
+
+    const size_t rect_w = win_w / map_w;
+    const size_t rect_h = win_h / map_h;
+    for (size_t j = 0; j < map_h; j++)
+    {
+        for (size_t i = 0; i < map_w; i++)
+        {
+            if (map[i + j * map_w] == ' ')
+            {
+                continue;
+            }
+
+            size_t rect_x = i * rect_w;
+            size_t rect_y = j * rect_h;
+
+            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, pack_color(0, 255, 255));
         }
     }
 
