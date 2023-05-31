0f0b08fd8060618e93c2864abb9facb9a702e2d0
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-31 11:34:09 +0200

    Refactor bench to have triangle name
    This is in preperation of having a raycasting and raytracing
    benchmark in.
    
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- bench_triangle.sh
+++ bench_triangle.sh
@@ -2,8 +2,8 @@
 
 clear
 
-bazel build -c opt //:bench
+bazel build -c opt //:bench_triangle
 
 sudo cpupower frequency-set --governor performance
-taskset 0x1 ./bazel-bin/bench
+taskset 0x1 ./bazel-bin/bench_triangle
 sudo cpupower frequency-set --governor powersave
