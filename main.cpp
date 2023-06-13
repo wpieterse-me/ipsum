b532183b7cab7db58062696249208f60d86eb160
Wynand Pieterse <limb@wpieterse.me>
2023-06-13 21:29:08 +0200

    Add vector file for math
    It's just a initial file, and a simple program that includes it.
    
    Signed-off-by: Wynand Pieterse <limb@wpieterse.me>

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <cstdint>
+#include <iostream>
+
+#include "vector.hpp"
+
+int32_t main(int32_t argument_count, char** arguments) {
+  std::cout << "Hello, World" << std::endl;
+
+  return 0;
+}
