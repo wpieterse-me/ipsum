90b95b03eda5b88de9a83bafde6c4a12fc2cf6d9
Wynand Pieterse <limb@wpieterse.me>
2023-06-13 22:10:46 +0200

    Add simple definition for Vectors with SIMD
    Signed-off-by: Wynand Pieterse <limb@wpieterse.me>

--- main.cpp
+++ main.cpp
@@ -4,7 +4,25 @@
 #include "vector.hpp"
 
 int32_t main(int32_t argument_count, char** arguments) {
-  std::cout << "Hello, World" << std::endl;
+  Vector_D32_4 lhs;
+
+  lhs.standard_storage[0] = 1.0f;
+  lhs.standard_storage[1] = 2.0f;
+  lhs.standard_storage[2] = 3.0f;
+  lhs.standard_storage[3] = 4.0f;
+
+  Vector_D32_4 rhs;
+
+  rhs.standard_storage[0] = 1.0f;
+  rhs.standard_storage[1] = 2.0f;
+  rhs.standard_storage[2] = 3.0f;
+  rhs.standard_storage[3] = 4.0f;
+
+  Vector_D32_4 result = lhs + rhs;
+
+  for(auto index = 0; index < 4; index++) {
+    std::cout << result.standard_storage[index] << std::endl;
+  }
 
   return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -1,1 +1,133 @@
 #pragma once
+
+#include <cstdint>
+
+#include <immintrin.h>
+
+#include <array>
+
+template <typename ElementType, size_t Dimensions>
+struct VectorSIMD {
+    using Type = std::array<ElementType, Dimensions>;
+};
+
+template <>
+struct VectorSIMD<float, 1> {
+    using Type = __m128;
+};
+
+template <>
+struct VectorSIMD<float, 2> {
+    using Type = __m128;
+};
+
+template <>
+struct VectorSIMD<float, 3> {
+    using Type = __m128;
+};
+
+template <>
+struct VectorSIMD<float, 4> {
+    using Type = __m128;
+};
+
+template <>
+struct VectorSIMD<float, 5> {
+    using Type = __m256;
+};
+
+template <>
+struct VectorSIMD<float, 6> {
+    using Type = __m256;
+};
+
+template <>
+struct VectorSIMD<float, 7> {
+    using Type = __m256;
+};
+
+template <>
+struct VectorSIMD<float, 8> {
+    using Type = __m256;
+};
+
+template <typename ElementType, size_t Dimensions>
+struct Vector {
+    using StandardStorage = std::array<ElementType, Dimensions>;
+    using SIMDStorage     = VectorSIMD<ElementType, Dimensions>::Type;
+
+    union {
+        StandardStorage standard_storage;
+        SIMDStorage     simd_storage;
+    };
+};
+
+template <typename ElementType, size_t Dimensions>
+void initialize(Vector<ElementType, Dimensions>& item) {
+  if constexpr(Dimensions > 0 && Dimensions <= 4) {
+    item.simd_storage = _mm_setzero_ps();
+
+    return;
+  }
+
+  if constexpr(Dimensions > 4 && Dimensions <= 8) {
+    item.simd_storage = _mm256_setzero_ps();
+
+    return;
+  }
+
+  for(size_t index = 0; index < Dimensions; index++) {
+    item.standard_storage[index] = ElementType{};
+  }
+}
+
+template <typename ElementType, size_t Dimensions>
+Vector<ElementType, Dimensions>
+operator+(const Vector<ElementType, Dimensions>& lhs,
+          const Vector<ElementType, Dimensions>& rhs) {
+  Vector<ElementType, Dimensions> result;
+
+  for(size_t index = 0; index < Dimensions; index++) {
+    auto lhs_value    = lhs.standard_storage[index];
+    auto rhs_value    = rhs.standard_storage[index];
+    auto result_value = lhs_value + rhs_value;
+
+    result.standard_storage[index] = result_value;
+  }
+
+  return result;
+}
+
+template <>
+Vector<float, 4> operator+(const Vector<float, 4>& lhs,
+                           const Vector<float, 4>& rhs) {
+  Vector<float, 4> result;
+
+  const auto lhs_value = lhs.simd_storage;
+  const auto rhs_value = rhs.simd_storage;
+
+  result.simd_storage = _mm_add_ps(lhs_value, rhs_value);
+
+  return result;
+}
+
+using dec_32 = float;
+using dec_64 = double;
+
+using Vector_D32_1 = Vector<dec_32, 1>;
+using Vector_D32_2 = Vector<dec_32, 2>;
+using Vector_D32_3 = Vector<dec_32, 3>;
+using Vector_D32_4 = Vector<dec_32, 4>;
+using Vector_D32_5 = Vector<dec_32, 5>;
+using Vector_D32_6 = Vector<dec_32, 6>;
+using Vector_D32_7 = Vector<dec_32, 7>;
+using Vector_D32_8 = Vector<dec_32, 8>;
+
+using Vector_D64_1 = Vector<dec_64, 1>;
+using Vector_D64_2 = Vector<dec_64, 2>;
+using Vector_D64_3 = Vector<dec_64, 3>;
+using Vector_D64_4 = Vector<dec_64, 4>;
+using Vector_D64_5 = Vector<dec_64, 5>;
+using Vector_D64_6 = Vector<dec_64, 6>;
+using Vector_D64_7 = Vector<dec_64, 7>;
+using Vector_D64_8 = Vector<dec_64, 8>;
