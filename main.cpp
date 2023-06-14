ac81c843e1a985e5f2235a68ddd059238c01634d
Wynand Pieterse <limb@wpieterse.me>
2023-06-14 23:44:21 +0200

    Remove old container code
    Signed-off-by: Wynand Pieterse <limb@wpieterse.me>

--- main.cpp
+++ main.cpp
@@ -1,23 +1,6 @@
 #include <cstdint>
 #include <iostream>
 
-#include "container.hpp"
-#include "vector.hpp"
-
 int32_t main(int32_t argument_count, char** arguments) {
-  dynamic_array_storage       storage{ sizeof(uint32_t), alignof(uint32_t) };
-  dynamic_array_view<int32_t> view{ storage };
-
-  view.push_back(int32_t{ 1 });
-  view.push_back(int32_t{ 2 });
-  view.push_back(int32_t{ 4 });
-  view.push_back(int32_t{ 8 });
-  view.push_back(int32_t{ 16 });
-  view.push_back(int32_t{ 32 });
-
-  for(auto item: view) {
-    std::cout << item << std::endl;
-  }
-
   return 0;
 }
