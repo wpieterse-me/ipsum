5c4a1a59d0c3ceb7b3217d0f5230fa056afc4f8a
Wynand Pieterse <limb@wpieterse.me>
2023-06-17 10:36:05 +0200

    Clean up root main file
    Signed-off-by: Wynand Pieterse <limb@wpieterse.me>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <cstdint>
 #include <iostream>
 
+class IFactory {
+  public:
+    virtual ~IFactory() {
+    }
+};
+
+class Factory : public IFactory {
+  public:
+};
+
 int32_t main(int32_t argument_count, char** arguments) {
+  std::cout << std::endl;
+  std::cout << "DONE!!!" << std::endl;
+  std::cout << std::endl;
+
   return 0;
 }
