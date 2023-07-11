7f22b301ae0dcf8352a556e6e0e3d643f1d2d36c
Wynand Pieterse <limb@wpieterse.me>
2023-07-11 16:37:41 +0200

    Make pipeline build Bazel targets before bench
    Signed-off-by: Wynand Pieterse <limb@wpieterse.me>

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -13,6 +13,14 @@ targets = [
 print("steps:")
 
 for target in targets:
+    print("  - label: \":hammer: Build ({})\"".format(target))
+    print("    command: \"bazel build -c opt //...\"")
+    print("    agents:")
+    print("      user: \"{}\"".format(target))
+
+print("  - wait")
+
+for target in targets:
     print("  - label: \":hammer: Run Bench ({})\"".format(target))
     print("    command: \"bazel run -c opt //:bench\"")
     print("    agents:")
